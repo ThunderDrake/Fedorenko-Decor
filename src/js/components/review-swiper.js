3ac5787abcd5c7955ca4e9f2a54df9c648a49506
Guihal <guihalbonik@gmail.com>
2023-02-26 17:54:43 +0300

    ref

--- src/js/components/review-swiper.js
+++ src/js/components/review-swiper.js
@@ -3,36 +3,22 @@ import Swiper from 'swiper/bundle';
 
 const swiper = new Swiper('.review__swiper',{
   direction: 'horizontal' ,
-  spaceBetween: 10,
-  slidesPerView: 3,
   navigation: {
     nextEl: '.swiper__next',
     prevEl: '.swiper__prev'
   },
+  slidesPerGroup: 1,
+  slidesPerView: 1,
+  spaceBetween: 16,
 
   pagination:{
     el: '.swiper__pagination',
     type: 'fraction',
-    formatFractionCurrent: function (number) {
-      return '0' + number;
+    formatFractionTotal: function (number) {
+      return number;
   }
   },
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 2,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 3,
-      spaceBetween: 40
-    }
-  }
+
+
+
 });

7f77f2ff8710d3ab26a07a915728a72c652609cf
Timofey Krivenkov <drake9100@mail.ru>
2023-03-12 20:39:12 +0300

    UPD: Секция отзывов

--- src/js/components/review-swiper.js
+++ src/js/components/review-swiper.js
@@ -1,18 +1,34 @@
 import Swiper from 'swiper/bundle';
+const sliderSectionContainer = document.querySelector('.review__container');
 
+let slidesOffset;
 
-const swiper = new Swiper('.review__swiper',{
-  direction: 'horizontal' ,
+if(window.matchMedia('(max-width: 768px)').matches) {
+  slidesOffset = 80;
+} else {
+  slidesOffset = 30;
+}
+
+const swiper = new Swiper('.review__slider',{
   navigation: {
-    nextEl: '.swiper__next',
-    prevEl: '.swiper__prev'
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
   },
-  slidesPerGroup: 1,
-  slidesPerView: 1,
-  spaceBetween: 16,
 
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      autoHeight: true
+    },
+    567: {
+      slidesPerView: 'auto',
+      spaceBetween: 16,
+      slidesOffsetBefore: (document.documentElement.clientWidth + slidesOffset - sliderSectionContainer.offsetWidth) / 2,
+      slidesOffsetAfter: (document.documentElement.clientWidth + slidesOffset - sliderSectionContainer.offsetWidth) / 2,
+    }
+  },
   pagination:{
-    el: '.swiper__pagination',
+    el: '.swiper-pagination',
     type: 'fraction',
     formatFractionTotal: function (number) {
       return number;
